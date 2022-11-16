566a7a5730355afe62e97bc6f7859fbe64811a8d
kelvinLin9 <kelvin80121@hotmail.com>
2022-11-16 15:09:30 +0800

    首頁建置中

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,81 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home d-flex justify-content-center align-items-center position-relative test">
+    <div class="main container position-relative">
+      <div class="free text-white d-flex justify-content-center align-items-center Noto-Sans-TC fs-24">
+            免費試用版
+      </div>
+      <div class="row">
+        <div class="col ps-5 pt-5 mt-5 Noto-Sans-TC">
+          <h1 class="fs-50 text-primary fw-bolder pt-5">
+            小綠簽
+          </h1>
+          <p class="">
+            護樹、永續、減碳的綠色生活<br>
+            響應環保無紙化電子簽署，<br>
+            省時便利又環保。
+          </p>
+        </div>
+        <div class="col Noto-Sans-TC">
+          <div class="input-pdf d-flex flex-column justify-content-center align-items-center mt-5">
+            <img src="../assets/images/pdf-input.png" alt="">
+            <div class="bg-gradient text-white px-5 py-2 rounded-3">
+              <input type="file" accept="application/pdf" placeholder="選擇PDF檔案" />
+            </div>
+            <p class="fs-16 my-0">或拖曳檔案到此處</p>
+            <p class="fs-14 text-primary">(限10MB 內的PDF或JPG檔)</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="bg-grassland-2"></div>
+    <div class="bg-people4"></div>
+    <div class="bg-people1"></div>
+    <div class="bg-people2"></div>
+    <div class="bg-people3"></div>
+    <div class="bg-potted-plant"></div>
+    <div class="bg-seed"></div>
+    <div class="bg-grassland-1"></div>
+    <div class="bg-home1"></div>
+    <div class="bg-home2"></div>
   </div>
+  3333<i class="fa-solid fa-tricycle"></i>
+  <i class="fa-sharp fa-solid fa-house"></i>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
 }
 </script>
+<style lang="scss" scoped>
+.home {
+  width: 1280px;
+  height: 720px;
+}
+.main {
+  background: rgba(240, 240, 240, 0.58);
+  width: 1036px;
+  height: 558px;
+  border-radius: 34px;
+}
+.free {
+  position: absolute;
+  width: 225px;
+  height: 62px;
+  top: 0%;
+  left: 0%;
+  background: linear-gradient(180deg, #35A483 0%, #077854 100%);
+  border-radius: 34px 0px;
+}
+.input-pdf {
+  width: 417px;
+  height: 376px;
+  padding: 20px 10px 82px;
+  gap: 20px;
+  background: #FFFFFF;
+  border: 2px dashed #B7B7B7;
+  border-radius: 26px;
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,24 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  // 修正檔案命名問題
+  lintOnSave: false,
+  publicPath: process.env.NODE_ENV === 'production' ? '/F2E_Week2/' : '/'
 })
+
+
+//
+module.exports = {
+  configureWebpack: {
+    externals: {
+      // CDN 的 Element 依赖全局变量 Vue， 所以 Vue 也需要使用 CDN 引入
+      vue: 'Vue',
+      // 属性名称 element-ui, 表示遇到 import xxx from 'element-ui' 这类引入 'element-ui'的，
+      // 不去 node_modules 中找，而是去找 全局变量 ELEMENT
+      fabric: 'fabric',
+      pdf: 'pdf',
+      'vue-router': 'VueRouter'
+    }
+  }
+}
